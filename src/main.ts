c636c2eb7ff3ce1ef6a1df85f26860814a1ff9f0
Thomas Eykamp <embergameslp@gmail.com>
2024-01-16 22:21:22 -0500

    add project files

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import { createPinia } from 'pinia'
+
+loadFonts()
+
+createApp(App).use(createPinia())
+  .use(vuetify)
+  .mount('#app')
