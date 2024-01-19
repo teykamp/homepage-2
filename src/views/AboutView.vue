172181f2c18d6ffc582e9fa632685fded3afee7f
Thomas Eykamp <embergameslp@gmail.com>
2024-01-19 01:02:20 -0500

    update navbar

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+
+    <CardAnimationWrapper :format="{
+      borderAnimation: true,
+      border: true,
+      hoverEffect: false
+    }">
+      <template #card-content>
+        <NavCardVue />
+      </template>
+    </CardAnimationWrapper>
+
   </div>
 </template>
+
+<script setup lang="ts">
+import CardAnimationWrapper from '../components/CardAnimationWrapper.vue';
+import NavCardVue from '../components/NavCard.vue';
+</script>
